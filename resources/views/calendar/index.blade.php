c17271f241cac20d90b099b9de5ae672c7149b27
FinnWiel <finnvandewiel01@gmail.com>
2024-06-24 11:09:10 +0200

    Update calendar page title to "Calendar"

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Home</title>
+    <title>Calendar</title>
     @vite('resources/css/app.css')
 
     <!-- CSRF Token -->
